14c85c1b9f06167dd113b1e96e042bd32b5b8250
factorgh <burchellsbale@gmail.com>
2025-02-07 11:18:01 +0000

    Update on tabs section

--- src/components/core/tabs.jsx
+++ src/components/core/tabs.jsx
@@ -1,18 +1,24 @@
-import { DollarSign, FileText, Home, Layers, Search, User } from "lucide-react";
-import { useState } from "react";
+import { FileText, Home, Layers, Search, User } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Tabs = () => {
   const is_subscribed = localStorage.getItem("is_subscribed") === "true";
-  console.log("is_subscribed:", is_subscribed);
-  const [activeTab, setActiveTab] = useState("home");
+
+  // Retrieve last active tab from localStorage, default to "home"
+  const [activeTab, setActiveTab] = useState(
+    localStorage.getItem("activeTab") || "home"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("activeTab", activeTab); // Store active tab on change
+  }, [activeTab]);
 
   const filter = [
     { name: "Home", value: "home", icon: <Home /> },
     { name: "Permits", value: "permits", icon: <FileText /> },
     { name: "Operator", value: "operator", icon: <User /> },
     { name: "Leases", value: "leases", icon: <Layers /> },
-    { name: "Pricing", value: "pricing", icon: <DollarSign /> },
     { name: "Search", value: "search", icon: <Search /> },
   ];
 
@@ -26,11 +32,14 @@ const Tabs = () => {
             to={isDisabled ? "#" : `/${item.value}`} // Prevent navigation if disabled
             key={item.value}
             onClick={() => {
-              if (!isDisabled) setActiveTab(item.value);
+              if (!isDisabled) {
+                setActiveTab(item.value);
+                localStorage.setItem("activeTab", item.value); // Save selected tab
+              }
             }}
             className={`flex items-center justify-center gap-1 cursor-pointer p-2 transition-all duration-300 ${
               isDisabled
-                ? "opacity-50 cursor-not-allowed" // Make it visually disabled
+                ? "opacity-50 cursor-not-allowed" // Visually disabled
                 : activeTab === item.value
                 ? "border-2 border-blue-500 bg-blue-200 text-blue-900 rounded-lg"
                 : "text-gray-600 hover:text-blue-500"
